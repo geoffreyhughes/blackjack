99ed76f58b71b5aaaaa0d07d78a2d8a915ad26fc
Geoffrey Hughes <linkmaster@me.com>
2020-12-03 00:44:04 -0800

    hit working for one hand (no split)

--- blackjack.js
+++ blackjack.js
@@ -54,27 +54,32 @@ function createPlayers(numPlayers)
 {
 	for (var i = 0; i < numPlayers; i++)
 	{
+		var hands = new Array();
 		var hand = new Array();
 
-		var player = {number: i, points: 0, hand: hand};
+		hands[0] = hand;
+
+		var player = {number: i, points: 0, hands: hands};
 
 		players.push(player);
 	}
 }
 
-function drawCard(playerNum)
+function drawCard(playerNum, handNumber)
 {
 	var drawnCard = deck.pop();
+
+	players[playerNum]["hands"][handNumber].push(drawnCard);
+	players[playerNum]["points"] += drawnCard["weight"];
+	console.log(deck.length + " cards left");
+	console.log("hands: " + players[playerNum]["hands"][handNumber]);
+	console.log(players[playerNum]["points"]);
+
+	
 
 	// may want to remove console log to prevent player from inspecting the hidden card element
 	console.log(drawnCard);
 	console.log(players[playerNum]);
-
-	players[playerNum]["hand"].push(drawnCard);
-	players[playerNum]["points"] += drawnCard["weight"];
-	console.log(deck.length + " cards left");
-	console.log("hand: " + players[playerNum]["hand"]);
-	console.log(players[playerNum]["points"]);
 }
 
 function displayCards()
@@ -82,31 +87,38 @@ function displayCards()
 
 	for (var j = 0; j < players.length; j++)
 	{
-		for (var i = 0; i < players[j]["hand"].length; i++)
+		for (var i = 0; i < players[j]["hands"].length; i++)
 		{
-			var rank = players[j]["hand"][i].rank;
-			var suit = players[j]["hand"][i].suit;
-
-			var img = document.createElement("img");
-			img.src="cards/" + rank + "_of_" + suit + ".png";
-			img.weight=120;
-			img.height=180;
-			img.id=players[i] + "_" + [j];
-			var divPlayer = document.getElementById("player" + players[j]["number"]);
-
-			if (j == 0 && i == 1)
+			for (var k = 0; k < players[j]["hands"][i].length; k++)
 			{
-				var back = document.createElement("img");
-				back.src="cards/GeoffsHouse.png";
-				back.weight=120;
-				back.height=180;
-				divPlayer.appendChild(back);
+
+				var rank = players[j]["hands"][i][k].rank;
+				var suit = players[j]["hands"][i][k].suit;
+	
+				var img = document.createElement("img");
+				img.src="cards/" + rank + "_of_" + suit + ".png";
+				img.weight=120;
+				img.height=180;
+				img.id="player" + players[j]["number"] + "_" + i + "_" + k;
+				console.log(img.id);
+				var divPlayer = document.getElementById("player" + players[j]["number"] + "_" + i);
+	
+				if (j == 0 && i == 0 && k == 1)
+				{
+					var back = document.createElement("img");
+					back.src="cards/GeoffsHouse.png";
+					back.weight=120;
+					back.height=180;
+					divPlayer.appendChild(back);
+				}
+	
+				else 
+				{
+					divPlayer.appendChild(img);
+				}
+
 			}
 
-			else 
-			{
-				divPlayer.appendChild(img);
-			}
 
 		}
 
@@ -118,6 +130,47 @@ function displayCards()
 
 }
 
+function clearCards()
+{
+	// may need to add hand based logic 
+	clearBox("player0_0");
+	clearBox("player1_0");
+}
+
+function clearBox(elementID)
+{
+    document.getElementById(elementID).innerHTML = "";
+}
+
+function hit(playerNumber, handNumber)
+{
+	console.log("HIT ME BABY ONE MORE TIME");
+	drawCard(playerNumber, handNumber);
+
+	var cardNumber = players[playerNumber]["hands"][handNumber].length;
+
+	console.log(players[playerNumber]["hands"][handNumber])
+
+	clearCards();
+	displayCards();
+	// // TODO: add hand logic with HTML, add card to the cooresponding hand that was hit
+
+}
+
+
+
+// function stay(playerNumber, handNumber)
+// {
+
+// }
+
+// function split(playerNumber, handNumber)
+// {
+
+// }
+
+
+
 // Creates deck, players, and deals initial cards
 function startGame()
 {
@@ -125,30 +178,13 @@ function startGame()
 	shuffleDeck();
 	createPlayers(2);
 
-	drawCard(0);
-	drawCard(1);
-	drawCard(0);
-	drawCard(1);
+	drawCard(0, 0);
+	drawCard(1, 0);
+	drawCard(0, 0);
+	drawCard(1, 0);
 
 
 	displayCards();
 }
-
-startGame();
-
-
-
-
-
-
-
-
-
-
-for (var i = 0; i < 48; i++)
-{
-	console.log(deck[i].weight)
-}
-
 
 
